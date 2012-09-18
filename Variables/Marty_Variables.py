74ad0241bbeb6bbbdd7dd3adf63ff553b7138aa4
Joah Gerstenberg <joahd@gbergfamily.com>
2012-09-18 16:24:27 -0500

    Still working on variables...

--- Variables/Marty_Variables.py
+++ Variables/Marty_Variables.py
@@ -5,14 +5,9 @@ class dotdict(dict):
     __delattr__= dict.__delitem__
 
 dd = dotdict()
-var = array([])
-a = raw_input()
-var.append(a)
-dd.a = raw_input()
 print dd.a
 print a
 
 b = raw_input()
 for i in var:
 	if b == i:
-		print dd.a
